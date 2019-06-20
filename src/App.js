99cfd94ea91228290657081d14f4c81b54299066
Axel <axosar@gmail.com>
2019-06-20 11:03:12 -0500

    The Ball
    Ball Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Ball from './Ball';
 
 function App() {
   return (
     <div className="App">
-
+      <Ball num={17} />
     </div>
   );
 }
